8b23547e0459b8e102d3f25afedef741641c5d0e
Jakob Persons <jakobep24@gmail.com>
2020-10-26 17:34:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,10 +23,8 @@ def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning(file_path, emoticon)
+def get_english_meaning(file_path, japanese_emoticon)
   # code goes here
-  load_library(file_path)
-  if emoticon_lib[key][:japanese].value?(emoticon)
-    emoticon_lib[key][:japanese]
+  load_library(file_path).key([:japanese] => japanese_emoticon)
   end
 end
