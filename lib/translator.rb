8373ea9a24ac98ba2465a6792f616a769e0d07c7
Jakob Persons <jakobep24@gmail.com>
2020-10-26 16:08:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,8 @@ def load_library(file_path)
   emoticons.each do |key, value|
     emoticon_lib[:key][:english] = value[0]
     emoticon_lib[:key][:japanese] = value[1]
-
+  end
+  emoticon_lib
 end
 
 
