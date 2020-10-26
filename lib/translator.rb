59390dd5641ec8c1e8d51f7eb96b266b66c0db82
Jakob Persons <jakobep24@gmail.com>
2020-10-26 15:45:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,19 +4,13 @@ require "yaml"
 
 def load_library(file_path)
   # code goes here
-  # set yaml file to be converted equal to variable
   emoticons = YAML.load_file('lib/emoticons.yml')
 
-  emoticon_lib = {
-    :emoticon_name => {}
-  }
-
-   emoticons.each do |key, value|  # key = emotion   # value = emoticon to go with emotion
-     emoticon_lib[:emoticon_name] = key
-     emoticon_lib[:emoticon_name][:english] = value[0]
-     emoticon_lib[:emoticon_name][:japanese] = value [1]
-   end
-   emoticon_lib
+  emoticons.each do |key, value|
+    emoticon_lib = {}
+    emoticon_lib[:key] = {value[0], value [1]}
+  end
+  emoticon_lib
 end
 
 
